09bb38c49d8bd3841d5300f88210ed699f7b4482
Aaron G <33606245+AaaronMaiden@users.noreply.github.com>
2018-12-28 17:09:52 +0100

    Update FrSkySPort2MAVLink.ino

--- ArduinoCode/FrSkySPort2MAVLink/FrSkySPort2MAVLink.ino
+++ ArduinoCode/FrSkySPort2MAVLink/FrSkySPort2MAVLink.ino
@@ -1,6 +1,9 @@
 /*
  * 
- *                       FRSKY SPORT 2 MAVLINK CONVERTER
+ *                        FRSKY SPORT 2 MAVLINK CONVERTER
+ *                       
+ * Arduino based FrSky SmartPort to MAVLink converter to display INAV telemetry 
+ * data in Ground Station App via Bluetooth
  * 
  * 
  * 
